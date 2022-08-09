2faedaad0dbd416eed884c8f3b4382808b8fa4a6
Lacharte <guillaume.chatriot@gmail.com>
2022-08-09 09:03:16 +0200

    Add files via upload
    html and css good enough add the rules box and try to "add the grid"

--- Puzzle-1.js
+++ Puzzle-1.js
@@ -1,2 +1,32 @@
+const button = document.getElementById('rulesButton'); // Assumes element with id='button'
 
-  
+
+
+// const grid = document.getElementById('grid')
+// const cells = []
+
+// let playerPosition = 23
+
+// console.log(grid)
+
+// function addCell() {
+//   const cell = document.createElement('div')
+//   cell.classList.add('cell')
+//   grid.append(cell)
+
+//   // keep the cell for later
+//   cells.push(cell)
+// }
+
+// function add100Cells() {
+//   for (let i = 0; i < 100; i++) {
+//     addCell()
+//   }
+// }
+
+add100Cells()
+// const rulesButton = document.getElementById('rulesButton')
+ 
+// button.addEventListener('click', function() {
+    
+// })
